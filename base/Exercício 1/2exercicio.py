5a5d8d116d3f248a399d9df54ee7ca18f6d4f70d
david <djunio239@gmail.com>
2024-10-19 16:41:14 -0300

    python

---
+++ base/Exercício 1/2exercicio.py
@@ -0,0 +1,14 @@
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+soma = numero_1 + numero_2
+print(f'A soma dos dois números é {soma}')
+
+
+numero = input('Digite um número: ')
+print(f'O número informado foi {numero}')
+
+
+type('Olá Mundo')
+
+
+print('Olá Mundo')
